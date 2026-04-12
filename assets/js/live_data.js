f5539d7ea55ac174fdfe294b271a68701fecbff1
balage-energetix <balazs.horvath.energy@gmail.com>
2026-04-12 17:05:51 +0200

    Add files via upload

---
+++ assets/js/live_data.js
@@ -0,0 +1,14 @@
+window.LIVE_DATA = {
+    news: [
+        { title: "Farsangi kavalkád a nagyatádi általános iskolákban", date: "2026.02.10.", link: "https://www.atadhir.hu/hirek/nagyatad/farsangi-kavalkad-a-nagyatadi-altalanos-iskolakban/" },
+        { title: "Megkezdték a fák metszését és a zöldterületek takarítását a városban", date: "2026.02.12.", link: "https://www.atadhir.hu/hirek/nagyatad/megkezdtek-a-fak-metszeset-es-a-zoldteruletek-takaritasat-a-varosban/" },
+        { title: "Tavaszi lomtalanítás lesz Nagyatádon", date: "2026.02.08.", link: "https://www.atadhir.hu/hirek/tavaszi-lomtalanitas-nagyatadon/" },
+        { title: "Éremesővel és jótékonysággal indította a 2026-os szezont a NUTE", date: "2026.02.05.", link: "https://www.atadhir.hu/hirek/eremesovel-es-jotekonysaggal-inditotta-a-2026-os-szezont-a-nute/" },
+        { title: "Kevesebb jelzés, súlyosabb esetek: Megtartotta éves tanácskozását a gyermekjóléti szolgálat és központ", date: "2026.02.09.", link: "https://www.atadhir.hu/hirek/kevesebb-jelzes-sulyosabb-esetek-megtartotta-eves-tanacskozasat-a-gyermekjoleti-szolgalat-es-kozpont/" },
+        { title: "Pályaorientációs napot tartottak a NASZI-ban", date: "2026.02.09.", link: "https://www.atadhir.hu/hirek/nagyatad/palyaorientacios-napot-tartottak-a-naszi-ban/" },
+        { title: "Tűz ütött ki az egyik benzinkút épületében Nagyatádon", date: "2026.02.09.", link: "https://www.atadhir.hu/hirek/nagyatad/tuz-utott-ki-az-egyik-benzinkut-epuleteben-nagyatadon/" },
+        { title: "Ezüstérem és ígéretes helyezések: Remekeltek judósaink a Zrinski Openen", date: "2026.02.09.", link: "https://www.atadhir.hu/hirek/sport-hirek/ezusterem-es-igeretes-helyezesek-remekeltek-judosaink-a-zrinski-openen/" },
+        { title: "Látogatási tilalom és kötelező maszkviselés lépett érvénybe a Nagyatádi Kórházban", date: "2026.02.02.", link: "https://www.atadhir.hu/hirek/latogatasi-tilalom-es-kotelezo-maszkviseles-lepett-ervenybe-a-nagyatadi-korhazban/" },
+        { title: "Elűzték a telet Nagyatádról", date: "2026.02.06.", link: "https://www.atadhir.hu/hirek/nagyatad/eluztek-a-telet-nagyatadrol/" }
+    ]
+};
